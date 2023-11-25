780f82ca5e0960653dd3547076453fadee9c68cb
m4ztec <manueldias97@hotmail.com>
2023-11-25 11:58:30 +0000

    .

---
+++ WebUI/WebUI/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace WebUI.Models;
+
+public class Product
+{
+public required string Id { get; set; }
+public required string Name { get; set; }
+public string? Description { get; set; }
+public string[] Categories { get; set; } = [];
+public string[] ShopsAvailable { get; set; } = [];
+public int? SupposedPrice { get; set; }
+}
