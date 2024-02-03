f1ca63778fec5d23672b1625e3720180f8a8f4fc
m4ztec <manueldias97@hotmail.com>
2024-02-03 14:14:23 +0000

    PoC

--- WebUI/WebUI/Models/InventoryProduct.cs
+++ WebUI/WebUI/Models/InventoryProduct.cs
@@ -1,5 +1,20 @@
-﻿namespace WebUI.Models;
+﻿using System.Xml.Linq;
+
+namespace WebUI.Models;
 
 public class InventoryProduct : Product
 {
+    public InventoryProduct() { }
+
+    public InventoryProduct(Product product)
+    {
+        Id = product.Id;
+        Name = product.Name;
+        Owner = product.Owner;
+        Description = product.Description;
+        Categories = product.Categories;
+        ShopsAvailable = product.ShopsAvailable;
+        SupposedPrice = product.SupposedPrice;
+        Quantity = product.Quantity;
+    }
 }
--- WebUI/WebUI/Models/ShoppingListProduct.cs
+++ WebUI/WebUI/Models/ShoppingListProduct.cs
@@ -2,4 +2,18 @@
 
 public class ShoppingListProduct : Product
 {
+    public ShoppingListProduct() { }
+
+    public ShoppingListProduct(Product product)
+    {
+        Id = product.Id;
+        Name = product.Name;
+        Owner = product.Owner;
+        Description = product.Description;
+        Categories = product.Categories;
+        ShopsAvailable = product.ShopsAvailable;
+        SupposedPrice = product.SupposedPrice;
+        Quantity = product.Quantity;
+    }
+
 }
